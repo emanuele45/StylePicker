819b978965229d30b9a7cebcbb41ce3bd8f70017
emanuele <emanuele45@gmail.com>
2015-01-22 11:49:03 +0100

    Added bootstrap and some basic tests

---
+++ tests/PickerTest.php
@@ -0,0 +1,26 @@
+<?php
+
+class PickerTest extends \PHPUnit_Framework_TestCase
+{
+	protected $picker = null;
+
+	public function setUp()
+	{
+		$this->picker = new StylePicker();
+	}
+
+	public function testKnown()
+	{
+		$styles = $this->picker->getAttributes();
+
+		$this->assertEquals(7, count($styles));
+
+		foreach ($styles as $key => $val)
+		{
+			$this->assertTrue(in_array($key, array(
+				'color', 'background', 'font-size', 'border-style', 'border-width',
+				'border-color', 'border-radius'
+			)), $key . 'not within the expected known tests');
+		}
+	}
+}
---
+++ tests/bootstrap.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @name      OpenImporter
+ * @copyright OpenImporter contributors
+ * @license   BSD http://opensource.org/licenses/BSD-3-Clause
+ *
+ * @version 1.0 Alpha
+ */
+
+/**
+ * Just define ELK and require the files
+ */
+if (!defined('BASEDIR'))
+	define('BASEDIR', __DIR__ . '/../..');
+
+require_once(__DIR__ . '/../src/StylePicker.class.php');
+
+/**
+ * Dummy function
+ */
+function loadLanguage($lang)
+{
+	// Just nothing
+}
